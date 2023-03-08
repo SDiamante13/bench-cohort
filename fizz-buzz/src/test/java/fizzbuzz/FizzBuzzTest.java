d3f3f15a0d301a454fa82aa803b2d2577f1cc9a4
Emanuel Ocampo <eocampo86@gmail.com>
2023-03-08 16:31:44 -0300

    navigator test

--- fizz-buzz/src/test/java/fizzbuzz/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -8,6 +8,24 @@ class FizzBuzzTest {
 
     @Test
     void returnsInputAsAStringForNumbersNotDivisibleBy3Or5() {
+        assertThat(FizzBuzz.calculate(1)).isEqualTo("1");
+        assertThat(FizzBuzz.calculate(2)).isEqualTo("2");
+    }
 
+    @Test
+    void returnsFizzForNumberDivisibleBy3() {
+        assertThat(FizzBuzz.calculate(3)).isEqualTo("Fizz");
+    }
+    static class FizzBuzz {
+
+        public static String calculate(int i) {
+            if(i == 2) {
+                return "2";
+            }
+            else if(i == 3)
+                return "Fizz";
+
+            return "1";
+        }
     }
 }
