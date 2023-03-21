b1ca31bd556639b9cda0b37bb04fc495f804d195
Steven Diamante <steven.diamante@kinandcarta.com>
2023-03-21 10:08:40 -0400

    Add Shopping Basket TDD kata.

---
+++ shopping-basket/src/test/java/shoppingbasket/SampleTest.java
@@ -0,0 +1,13 @@
+package shoppingbasket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SampleTest {
+
+    @Test
+    void name() {
+        assertThat(true).isFalse();
+    }
+}
