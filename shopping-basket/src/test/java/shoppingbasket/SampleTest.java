f474c071296f8d36959228ff0741052c51ffd1b6
Emanuel Ocampo <eocampo86@gmail.com>
2023-03-22 14:54:13 -0300

    second test

--- shopping-basket/src/test/java/shoppingbasket/SampleTest.java
+++ shopping-basket/src/test/java/shoppingbasket/SampleTest.java
@@ -7,7 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SampleTest {
 
     @Test
-    void name() {
-        assertThat(true).isFalse();
+    void givenAnEmptyBasketWhenCalculateTotalThenReturn0() {
+
+        assertThat(ShoppingBasket.getTotal()).isEqualTo(0);
+    }
+
+    @Test
+    void givenAnEmptyBasketWithAnItemWhenFindingReturnTrue() {
+
+        assertThat(ShoppingBasket.getTotal()).isEqualTo(0);
+    }
+
+    private static class ShoppingBasket {
+        public static int getTotal() {
+            return 0;
+        }
     }
 }
