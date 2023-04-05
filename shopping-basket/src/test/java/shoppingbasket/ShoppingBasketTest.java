ab400fa87eba7dff4c3cf2773c50991d560e35f6
Steven Diamante <steven.diamante@kinandcarta.com>
2023-04-04 22:30:38 -0400

    Write out test cases

--- shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
+++ shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
@@ -6,35 +6,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ShoppingBasketTest {
 
+    /*
+    shouldHaveZeroTotalPriceForNoItems
+    0 items -> $0
+
+    shouldCalculateTotalPriceOfOneItem
+    1 item of $10 -> $10
+
+    shouldCalculateTotalPriceOfTwoItems
+    2 items of $5 and $6 -> $11
+
+    shouldCalculateTotalPriceOfMultipleItemsOfSameName
+    1 item with quantity of 5 $20 -> $100
+
+    shouldApply5PercentDiscountForOrderOver100
+    2 steaks of $40 each and 3 boxes of cereal $7 -> $80 + $21 = $101 * 95% = $95.95
+
+    shouldApply10PercentDiscountForOrderOver200
+    1 TV of $220 -> $198
+
+    shouldFindOutTheQuantityOfAParticularItem
+    item has 1 apple, 2 orange, and 3 bananas -> find each quantity
+    */
+
+
     @Test
     void shouldCalculateTotalPriceForNoItems() {
-    }
-
-    @Test
-    void shouldCalculateTotalPriceOfOneItem() {
-    }
-
-    @Test
-    void shouldCalculateTotalPriceOfTwoItems() {
-
-    }
-
-    @Test
-    void shouldCalculateTotalPriceOfMultipleItemsOfSameName() {
-
-    }
-
-    @Test
-    void shouldCalculateTotalPriceAppliesDiscountForOrderOver100() {
-
-    }
-
-    @Test
-    void shouldCalculateTotalPriceAppliesDiscountForOrderOver200() {
-
-    }
-
-    @Test
-    void shouldFindOutTheQuantityOfAParticularItem() {
     }
 }
