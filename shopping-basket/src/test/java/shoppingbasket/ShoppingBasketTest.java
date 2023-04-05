c5ee6cb1d173a8e2a589aef979dedb04e9946d88
Alejandro Meneses <almenesesp@gmail.com>
2023-04-05 12:44:09 -0500

    more tests

--- shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
+++ shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
@@ -2,6 +2,7 @@ package shoppingbasket;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,30 +11,6 @@ import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ShoppingBasketTest {
-
-    /*
-    shouldHaveZeroTotalPriceForNoItems
-    0 items -> $0
-
-    shouldCalculateTotalPriceOfOneItem
-    1 item of $10 -> $10
-
-    shouldCalculateTotalPriceOfTwoItems
-    2 items of $5 and $6 -> $11
-
-    shouldCalculateTotalPriceOfMultipleItemsOfSameName
-    1 item with quantity of 5 $20 -> $100
-
-    shouldApply5PercentDiscountForOrderOver100
-    2 steaks of $40 each and 3 boxes of cereal $7 -> $80 + $21 = $101 * 95% = $95.95
-
-    shouldApply10PercentDiscountForOrderOver200
-    1 TV of $220 -> $198
-
-    shouldFindOutTheQuantityOfAParticularItem
-    item has 1 apple, 2 orange, and 3 bananas -> find each quantity
-    */
-
 
     @Test
     void shouldCalculateTotalPriceForNoItems() {
@@ -47,9 +24,24 @@ class ShoppingBasketTest {
     @Test
     void shouldCalculateTotalPriceOfOneItem() {
         //1 item of $10 -> $10
-        ShoppingBasket shoppingBasket = new ShoppingBasket();
+        Map<String, Item> oneItem = new HashMap<>();
+        oneItem.put("Bread", new Item(10, 1));
+        ShoppingBasket shoppingBasket = new ShoppingBasket(oneItem);
         int total = shoppingBasket.calculateTotal();
         assertThat(total).isEqualTo(10);
+    }
+
+    @Test
+    void shouldCalculateTotalPriceOfTwoItems() {
+        Map<String, Item> twoItems = new HashMap<>();
+        twoItems.put("Bread", new Item(6, 1));
+        twoItems.put("Butter", new Item(5, 1));
+
+        ShoppingBasket shoppingBasket = new ShoppingBasket(twoItems);
+        int total = shoppingBasket.calculateTotal();
+
+        assertThat(total).isEqualTo(11);
+
     }
 
     private class ShoppingBasket {
@@ -61,7 +53,16 @@ class ShoppingBasketTest {
         }
 
         public int calculateTotal() {
-            return 0;
+            if(items.isEmpty()) {
+                return 0;
+            }
+
+            int total = 0;
+            for (Item item: items.values()) {
+                total += item.price;
+            }
+
+            return total;
         }
     }
 
