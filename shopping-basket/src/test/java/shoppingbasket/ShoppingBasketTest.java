91a2eb69e4e47943f046426d60921ab291e6c594
Gabriel Pagella <gabriel.pagella@kinandcarta.com>
2023-04-05 15:04:06 -0300

    new test method

--- shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
+++ shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
@@ -47,8 +47,23 @@ class ShoppingBasketTest {
         oneItem.put("Bread", new Item(20, 5));
 
         ShoppingBasket shoppingBasket = new ShoppingBasket(oneItem);
+        int total = shoppingBasket.calculateTotal();
 
+        assertThat(total).isEqualTo(100);
     }
+
+//    @Test
+//    void shouldApply5PercentDiscountForOrderOver100() {
+//        // 2 steaks of $40 each and 3 boxes of cereal $7 -> $80 + $21 = $101 * 95% = $95.95
+//        Map<String, Item> twoItems = new HashMap<>();
+//        twoItems.put("Steak", new Item(40, 2));
+//        twoItems.put("Cereal", new Item(7, 3));
+//
+//        ShoppingBasket shoppingBasket = new ShoppingBasket(twoItems);
+//        int total = shoppingBasket.calculateTotal();
+//
+//        assertThat(total).isEqualTo(95.95);
+//    }
 
     private class ShoppingBasket {
 
@@ -61,11 +76,13 @@ class ShoppingBasketTest {
         public int calculateTotal() {
             int total = 0;
             for (Item item : items.values()) {
-                total += item.getPrice();
+                total += item.calculateSubTotal();
             }
+
 
             return total;
         }
+
     }
 
 
@@ -74,17 +91,13 @@ class ShoppingBasketTest {
 
         private int quantity;
 
-        public int getPrice() {
-            return price;
-        }
-
-        public int getQuantity() {
-            return quantity;
-        }
-
         public Item(int price, int quantity) {
             this.price = price;
             this.quantity = quantity;
+        }
+
+        private int calculateSubTotal() {
+            return price * quantity;
         }
     }
 
