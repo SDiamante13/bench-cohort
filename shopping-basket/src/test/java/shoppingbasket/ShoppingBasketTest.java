bd6cb3f5fc349c008c7f9fdb10c2e146f978e6fe
Steven Diamante <steven.diamante@kinandcarta.com>
2023-04-04 21:18:29 -0400

    Setting up prenamed test cases and examples.

---
+++ shopping-basket/src/test/java/shoppingbasket/ShoppingBasketTest.java
@@ -0,0 +1,40 @@
+package shoppingbasket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShoppingBasketTest {
+
+    @Test
+    void shouldCalculateTotalPriceForNoItems() {
+    }
+
+    @Test
+    void shouldCalculateTotalPriceOfOneItem() {
+    }
+
+    @Test
+    void shouldCalculateTotalPriceOfTwoItems() {
+
+    }
+
+    @Test
+    void shouldCalculateTotalPriceOfMultipleItemsOfSameName() {
+
+    }
+
+    @Test
+    void shouldCalculateTotalPriceAppliesDiscountForOrderOver100() {
+
+    }
+
+    @Test
+    void shouldCalculateTotalPriceAppliesDiscountForOrderOver200() {
+
+    }
+
+    @Test
+    void shouldFindOutTheQuantityOfAParticularItem() {
+    }
+}
