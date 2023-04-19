2777d5a6696463368a36968fcb351b7037b4a747
Steven Diamante <steven.diamante@kinandcarta.com>
2023-04-18 23:09:08 -0400

    Add Game of Life kata.

---
+++ game-of-life/src/test/java/gameoflife/CellTest.java
@@ -0,0 +1,13 @@
+package gameoflife;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CellTest {
+
+    @Test
+    void name() {
+        assertThat(1).isEqualTo(1);
+    }
+}
