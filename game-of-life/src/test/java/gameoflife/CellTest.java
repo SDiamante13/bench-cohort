0536e4f6bcec05d7c0c2f7d545638dd1f978c6e5
carlos campo <carlos.campo@kinandcarta.com>
2023-04-20 13:08:59 -0500

    test one

--- game-of-life/src/test/java/gameoflife/CellTest.java
+++ game-of-life/src/test/java/gameoflife/CellTest.java
@@ -1,6 +1,6 @@
 package gameoflife;
 
-import com.sun.imageio.plugins.tiff.TIFFFaxCompressor;
+// import com.sun.imageio.plugins.tiff.TIFFFaxCompressor;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,13 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CellTest {
 
     @Test
-    void name() {
+    void cellWithOneLiveNeighborDies() {
         Cell cell = new Cell();
         assertThat(cell.nextState(1)).isEqualTo(CellState.DEAD);
     }
 
     private class Cell {
-        public CellState nextState(int neighbors) {
+        public CellState nextState(int liveNeighbors) {
+            return CellState.DEAD;
         }
     }
 }
