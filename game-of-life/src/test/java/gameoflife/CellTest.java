730b42f2b5e1e02b122fd2aa3fb3abb88113405a
Alejandro Meneses <almenesesp@gmail.com>
2023-04-20 13:15:45 -0500

    second test

--- game-of-life/src/test/java/gameoflife/CellTest.java
+++ game-of-life/src/test/java/gameoflife/CellTest.java
@@ -8,12 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CellTest {
 
     @Test
-    void cellWithOneLiveNeighborDies() {
-        Cell cell = new Cell();
+    void deadCellWithOneLiveNeighborDies() {
+        Cell cell = new Cell(CellState.DEAD);
         assertThat(cell.nextState(1)).isEqualTo(CellState.DEAD);
     }
 
+    @Test
+    void deadCellWithTwoLiveNeighborDies() {
+        Cell cell = new Cell(CellState.DEAD);
+        assertThat(cell.nextState(2)).isEqualTo(CellState.DEAD);
+    }
     private class Cell {
+
+        public Cell(CellState cellState) {
+
+        }
+
         public CellState nextState(int liveNeighbors) {
             return CellState.DEAD;
         }
