bcb9dc46986b36bf694d4c2f41eddd967503d668
Steven Diamante <steven.diamante@kinandcarta.com>
2023-05-31 13:56:07 -0400

    Cover with tests.

--- legacy-code/src/main/java/tripservice/trip/TripService.java
+++ legacy-code/src/main/java/tripservice/trip/TripService.java
@@ -2,6 +2,7 @@ package tripservice.trip;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 import tripservice.exception.UserNotLoggedInException;
 import tripservice.user.User;
@@ -10,8 +11,12 @@ import tripservice.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = UserSession.getInstance().getLoggedUser();
+		return getTripsByUser(user, loggedUser);
+	}
+
+	List<Trip> getTripsByUser(User user, User loggedUser) {
+		List<Trip> tripList = new ArrayList<Trip>();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -21,12 +26,16 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
+				tripList = findTripsBy(user);
 			}
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
+
+	List<Trip> findTripsBy(User user) {
+		return TripDAO.findTripsByUser(user);
+	}
+
 }
--- legacy-code/src/test/java/tripservice/trip/TripServiceShould.java
+++ legacy-code/src/test/java/tripservice/trip/TripServiceShould.java
@@ -1,5 +1,65 @@
 package tripservice.trip;
 
-public class TripServiceShould {
-	
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import tripservice.exception.UserNotLoggedInException;
+import tripservice.user.User;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+class TripServiceShould {
+
+    private TripService tripService = Mockito.spy(new TripService());
+
+    @Test
+    void returnsNoTripsForASearchedUserWithNoFriends() {
+        User searchedUser = new User();
+        User loggedInUser = new User();
+
+        List<Trip> trips = tripService.getTripsByUser(searchedUser, loggedInUser);
+
+        assertThat(trips).isEmpty();
+    }
+
+    @Test
+    void throwsExceptionWhenTheUserIsNotLoggedIn() {
+        User searchedUser = new User();
+        User notLoggedInUser = null;
+
+        assertThrows(UserNotLoggedInException.class,
+                () -> tripService.getTripsByUser(searchedUser, notLoggedInUser));
+    }
+
+    @Test
+    void returnsNoTripsWhenSearchedUserIsNotFriendsWithLoggedInUser() {
+        User searchedUser = new User();
+        User loggedInUser = new User();
+        searchedUser.addFriend(new User());
+
+        List<Trip> trips = tripService.getTripsByUser(searchedUser, loggedInUser);
+
+        assertThat(trips).isEmpty();
+    }
+
+    @Test
+    void isFriends() {
+        User searchedUser = new User();
+        User loggedInUser = new User();
+        searchedUser.addFriend(loggedInUser);
+
+
+        List<Trip> expectedTrips = List.of(new Trip());
+        doReturn(expectedTrips)
+                .when(tripService)
+                .findTripsBy(any(User.class));
+        List<Trip> trips = tripService.getTripsByUser(searchedUser, loggedInUser);
+
+        assertThat(trips).isEqualTo(expectedTrips);
+    }
 }
